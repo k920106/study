23689182164cd40e5daf776be6fd7d4e7419ff91
kangminsung <kk920106@naver.com>
2019-08-05 19:43:22 +0900

    2019 / 8 / 5 commit

---
+++ src/main/java/com/jojoldu/webservice/sign/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.jojoldu.webservice.sign.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    @Autowired
+//    User
+
+}
