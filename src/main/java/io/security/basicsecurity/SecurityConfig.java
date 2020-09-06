f1e88e379dbfd562ed2270fa129dd0ec64a184d3
kangminsung <kmsmk920106@gmail.com>
2020-09-06 23:51:56 +0900

    세번째 커밋

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,29 +1,34 @@
 package io.security.basicsecurity;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  @Autowired
-  private UserDetailsService userDetailsService;
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
+    auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("USER", "SYS");
+//    auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS");
+    auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("USER", "SYS", "ADMIN");
+//    auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN");
+  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
             .authorizeRequests()
+            .antMatchers("/user").hasRole("USER")
+            .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
+            .antMatchers("/admin/pay").hasRole("ADMIN")
             .anyRequest().authenticated();
     http
             .formLogin();
-    http
-            .rememberMe()
-            .userDetailsService(userDetailsService);
   }
 
 }
