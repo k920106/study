84fbe1389d1e6b1d8695491a2ab17333fd292988
kk920106 <kk920106@naver.com>
2024-01-25 08:56:13 +0900

    HTTP Status Code 제어

--- src/main/java/kr/co/kangminsung/myrestfulservice/controller/UserController.java
+++ src/main/java/kr/co/kangminsung/myrestfulservice/controller/UserController.java
@@ -2,8 +2,11 @@ package kr.co.kangminsung.myrestfulservice.controller;
 
 import kr.co.kangminsung.myrestfulservice.dao.User;
 import kr.co.kangminsung.myrestfulservice.dao.UserDaoService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,7 +28,14 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                                                  .path("/{id}")
+                                                  .buildAndExpand(savedUser.getId())
+                                                  .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
