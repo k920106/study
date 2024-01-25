13e12bf171eacd01419f24e6844f320595b1cbd1
kk920106 <kk920106@naver.com>
2024-01-25 15:36:56 +0900

    다국어 처리를 위한 Internationalization 구현 방법

--- src/main/java/kr/co/kangminsung/myrestfulservice/MyRestfulServiceApplication.java
+++ src/main/java/kr/co/kangminsung/myrestfulservice/MyRestfulServiceApplication.java
@@ -3,6 +3,11 @@ package kr.co.kangminsung.myrestfulservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class MyRestfulServiceApplication {
@@ -18,4 +23,10 @@ public class MyRestfulServiceApplication {
 //		}
 	}
 
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
 }
