d04b4eaa0e6b631dde7c178ec8a3a49d9a1158ec
kk920106 <kk920106@naver.com>
2024-01-25 09:16:46 +0900

    HTTP Status Code 제어를 위한 Exception Handling

--- src/main/java/kr/co/kangminsung/myrestfulservice/controller/UserController.java
+++ src/main/java/kr/co/kangminsung/myrestfulservice/controller/UserController.java
@@ -2,6 +2,7 @@ package kr.co.kangminsung.myrestfulservice.controller;
 
 import kr.co.kangminsung.myrestfulservice.dao.User;
 import kr.co.kangminsung.myrestfulservice.dao.UserDaoService;
+import kr.co.kangminsung.myrestfulservice.exception.UserNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -24,7 +25,13 @@ public class UserController {
 
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
-        return service.findOne(id);
+        User user = service.findOne(id);
+
+        if(user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        return user;
     }
 
     @PostMapping("/users")
