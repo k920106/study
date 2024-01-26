fb7662682978fef6e2e1f63440233198c2d52833
kk920106 <kk920106@naver.com>
2024-01-26 12:19:19 +0900

    Level3 단계의 REST API 구현을 위한 HATEOAS 적용

--- src/main/java/kr/co/kangminsung/myrestfulservice/controller/UserController.java
+++ src/main/java/kr/co/kangminsung/myrestfulservice/controller/UserController.java
@@ -4,12 +4,17 @@ import jakarta.validation.Valid;
 import kr.co.kangminsung.myrestfulservice.dao.User;
 import kr.co.kangminsung.myrestfulservice.dao.UserDaoService;
 import kr.co.kangminsung.myrestfulservice.exception.UserNotFoundException;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserController {
@@ -25,14 +30,20 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+//    public User retrieveUser(@PathVariable int id) {
+    public EntityModel retrieveUser(@PathVariable int id) {
         User user = service.findOne(id);
 
         if(user == null) {
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
         }
 
-        return user;
+        EntityModel entityModel = EntityModel.of(user);
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(linkTo.withRel("all-users"));
+
+//        return user;
+        return entityModel;
     }
 
     @PostMapping("/users")
