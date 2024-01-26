5c2ff2e089e434a0293d287834c8cb9a2596aaf3
kk920106 <kk920106@naver.com>
2024-01-26 17:50:18 +0900

    JPA 사용을 위한 Dependency 추가와 Entity 설정

--- src/main/java/kr/co/kangminsung/myrestfulservice/dao/User.java
+++ src/main/java/kr/co/kangminsung/myrestfulservice/dao/User.java
@@ -2,6 +2,9 @@ package kr.co.kangminsung.myrestfulservice.dao;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -13,8 +16,11 @@ import java.util.Date;
 @AllArgsConstructor
 @JsonIgnoreProperties(value={"password", "ssn"})
 @Schema(description = "사용자 상세 정보를 위한 도메인 객체")
+@Entity(name="users")
 public class User {
     @Schema(title = "사용자 ID", description = "사용자 ID는 자동 생성됩니다.")
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Schema(title = "사용자 이름", description = "사용자 이름을 입력합니다.")
