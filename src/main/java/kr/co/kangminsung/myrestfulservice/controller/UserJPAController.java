9564b2a2a63497949b6dd8844d94955b2aab1b2c
kk920106 <kk920106@naver.com>
2024-01-28 14:11:25 +0900

    JPA를 이용한 개별 사용자 상세 조회 - HTTP Get method

--- src/main/java/kr/co/kangminsung/myrestfulservice/controller/UserJPAController.java
+++ src/main/java/kr/co/kangminsung/myrestfulservice/controller/UserJPAController.java
@@ -1,12 +1,21 @@
 package kr.co.kangminsung.myrestfulservice.controller;
 
 import kr.co.kangminsung.myrestfulservice.dao.User;
+import kr.co.kangminsung.myrestfulservice.exception.UserNotFoundException;
 import kr.co.kangminsung.myrestfulservice.repository.UserRepository;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/jpa")
@@ -20,5 +29,21 @@ public class UserJPAController {
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity retrieveUser(@PathVariable int id) {
+        Optional<User> user = userRepository.findById(id);
+
+        if (!user.isPresent()) {
+            throw new UserNotFoundException("id-" + id);
+        }
+
+        EntityModel entityModel = EntityModel.of(user.get());
+
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(linkTo.withRel("all-users"));
+
+        return ResponseEntity.ok(entityModel);
     }
 }
