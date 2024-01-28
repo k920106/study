ce55819f25c3f1f463f6691ea8a836f2c6547eac
kk920106 <kk920106@naver.com>
2024-01-28 14:45:34 +0900

    JPA를 이용한 사용자 추가와 삭제 - HTTP POST/DELETE method

--- src/main/java/kr/co/kangminsung/myrestfulservice/controller/UserJPAController.java
+++ src/main/java/kr/co/kangminsung/myrestfulservice/controller/UserJPAController.java
@@ -1,16 +1,16 @@
 package kr.co.kangminsung.myrestfulservice.controller;
 
+import jakarta.validation.Valid;
 import kr.co.kangminsung.myrestfulservice.dao.User;
 import kr.co.kangminsung.myrestfulservice.exception.UserNotFoundException;
 import kr.co.kangminsung.myrestfulservice.repository.UserRepository;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -45,5 +45,21 @@ public class UserJPAController {
         entityModel.add(linkTo.withRel("all-users"));
 
         return ResponseEntity.ok(entityModel);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        userRepository.deleteById(id);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        User savedUser = userRepository.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                                                  .path("/{id}")
+                                                  .buildAndExpand(savedUser.getId())
+                                                  .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
