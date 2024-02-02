665d1cbb0ebbc90f583df85f9ef7e5de2c3ba0aa
kk920106 <kk920106@naver.com>
2024-02-02 16:19:27 +0900

    회원 서비스 테스트

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 
 public class MemberService {
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    //
     private final MemberRepository memberRepository;
 
     public MemberService(MemberRepository memberRepository) {
