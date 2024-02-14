78c5717341e02291fe5b0a3f620e98cd769de2fd
kk920106 <kk920106@naver.com>
2024-02-14 11:59:28 +0900

    관심사의 분리

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -9,16 +9,19 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 public class OrderApp {
-     public static void main(String[] args) {
-         MemberService memberService = new MemberServiceImpl();
-         OrderService orderService = new OrderServiceImpl();
+    public static void main(String[] args) {
+//        MemberService memberService = new MemberServiceImpl();
+//        OrderService orderService = new OrderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
-         long memberId = 1L;
-         Member member = new Member(memberId, "memberA", Grade.VIP);
-         memberService.join(member);
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
 
-         Order order = orderService.createOrder(memberId, "itemA", 10000);
-         System.out.println("order = " + order);
-//         System.out.println("order.calculatePrice = " + order.calculatePrice());
-     }
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        System.out.println("order = " + order);
+//        System.out.println("order.calculatePrice = " + order.calculatePrice());
+    }
 }
