2cc8fdfb637fd968744cc72e94688552071b4f42
kk920106 <kk920106@naver.com>
2024-02-16 13:10:09 +0900

    생성자 주입을 선택해라!

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -9,8 +9,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
-//    MemberService memberService = new MemberServiceImpl();
-//    OrderService orderService = new OrderServiceImpl();
     MemberService memberService;
     OrderService orderService;
 
@@ -26,7 +24,14 @@ public class OrderServiceTest {
         long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
+
         Order order = orderService.createOrder(memberId, "itemA", 10000);
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+    @Test
+    void createOrder2() {
+//        OrderServiceImpl orderService = new OrderServiceImpl();
+//        orderService.createOrder(1L, "itemA", 10000);
     }
 }
