a2c8ce3a9f728f8bd15c812cc0bc5a31f1da4b7e
kk920106 <kk920106@naver.com>
2024-02-20 15:41:42 +0900

    Synchronized 이용해보기

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -14,8 +14,9 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    @Transactional
-    public void decrease(Long id, Long quantity) {
+//    @Transactional
+//    public void decrease(Long id, Long quantity) {
+    public synchronized void decrease(Long id, Long quantity) {
         Stock stock = stockRepository.findById(id).orElseThrow();
 
         stock.decrease(quantity);
