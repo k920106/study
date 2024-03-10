c81bf3b20b03352d8f4a96d862572a7cea35f5b3
kk920106 <kk920106@naver.com>
2024-03-10 10:05:21 +0900

    본인계좌목록보기 서비스 만들기

--- src/main/java/com/bank/www/service/AccountService.java
+++ src/main/java/com/bank/www/service/AccountService.java
@@ -7,11 +7,16 @@ import com.bank.www.domain.user.UserRepository;
 import com.bank.www.dto.account.AccountReqDto.AccountSaveReqDto;
 import com.bank.www.dto.account.AccountRespDto.AccountSaveRespDto;
 import com.bank.www.handler.ex.CustomApiException;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -19,6 +24,40 @@ import java.util.Optional;
 public class AccountService {
     private final UserRepository userRepository;
     private final AccountRepository accountRepository;
+
+    public AccountListRespDto 계좌목록보기_유저별(Long userId) {
+        User userPS = userRepository.findById(userId).orElseThrow(() -> new CustomApiException("유저를 찾을 수 없습니다"));
+
+        // 유저의 모든 계좌목록
+        List<Account> accountListPS = accountRepository.findByUser_id(userId);
+        return new AccountListRespDto(userPS, accountListPS);
+    }
+
+    @Setter
+    @Getter
+    public static class AccountListRespDto {
+        private String fullname;
+        private List<AccountDto> accounts = new ArrayList<>();
+
+        public AccountListRespDto(User user, List<Account> accounts) {
+            this.fullname = user.getFullname();
+            this.accounts = accounts.stream().map(AccountDto::new).collect(Collectors.toList());
+        }
+
+        @Setter
+        @Getter
+        public class AccountDto {
+            private Long id;
+            private Long number;
+            private Long balance;
+
+            public AccountDto(Account account) {
+                this.id = account.getId();
+                this.number = account.getNumber();
+                this.balance = account.getBalance();
+            }
+        }
+    }
 
     @Transactional
     public AccountSaveRespDto 계좌등록(AccountSaveReqDto accountSaveReqDto, Long userId) {
@@ -37,38 +76,4 @@ public class AccountService {
         // DTO를 응답
         return new AccountSaveRespDto(accountPS);
     }
-
-//    @Setter
-//    @Getter
-//    public static class AccountSaveReqDto {
-//        @NotNull
-//        @Digits(integer = 4, fraction = 4)
-//        private Long number;
-//        @NotNull
-//        @Digits(integer = 4, fraction = 4)
-//        private Long password;
-//
-//        public Account toEntity(User user) {
-//            return Account.builder()
-//                          .number(number)
-//                          .password(password)
-//                          .balance(1000L)
-//                          .user(user)
-//                          .build();
-//        }
-//    }
-
-//    @Setter
-//    @Getter
-//    public static class AccountSaveRespDto {
-//        private Long id;
-//        private Long number;
-//        private Long balance;
-//
-//        public AccountSaveRespDto(Account account) {
-//            this.id = account.getId();
-//            this.number = account.getNumber();
-//            this.balance = account.getBalance();
-//        }
-//    }
 }
