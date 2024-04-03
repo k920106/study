29bff72b6e069fd175e529833584c3b3fe298e5a
kk920106 <kk920106@naver.com>
2024-04-03 22:43:38 +0900

    Hello JPA - 애플리케이션 개발

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,34 @@
+package hellojpa;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+            List<Member> resultList = em.createQuery("SELECT m FROM Member AS m", Member.class)
+                                        .setFirstResult(5)
+                                        .setMaxResults(8)
+                                        .getResultList();
+            for (Member member : resultList) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
