631121dfd0d162aa39d0c933b1f4710cc0456bcd
kk920106 <kk920106@naver.com>
2024-04-13 12:26:17 +0900

    값 타입의 비교

--- src/main/java/hellojpa/Address.java
+++ src/main/java/hellojpa/Address.java
@@ -3,6 +3,8 @@ package hellojpa;
 import jakarta.persistence.Embeddable;
 import lombok.*;
 
+import java.util.Objects;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
@@ -15,4 +17,17 @@ public class Address {
     private String street;
 
     private String zipcode;
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Address address = (Address) object;
+        return Objects.equals(city, address.city) && Objects.equals(street, address.street) && Objects.equals(zipcode, address.zipcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, zipcode);
+    }
 }
