d0fb281e2b8f08c6aabb66b73004fbabe87d00c3
kk920106 <kk920106@naver.com>
2024-04-21 23:01:09 +0900

    [자바 ORM 표준 JPA 프로그래밍 - 기본편] JPQL 함수

--- kimyounghan/jpashop0/jpql/src/main/java/com/example/jpql/jpql/JpaMain.java
+++ kimyounghan/jpashop0/jpql/src/main/java/com/example/jpql/jpql/JpaMain.java
@@ -19,9 +19,7 @@ public class JpaMain {
             em.persist(team);
 
             Member member = new Member();
-//            member.setUsername("member1");
-//            member.setUsername(null);
-            member.setUsername("관리자");
+            member.setUsername("member1");
             member.setAge(10);
             member.setType(MemberType.ADMIN);
 
@@ -29,16 +27,18 @@ public class JpaMain {
 
             em.persist(member);
 
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setAge(10);
+            member2.setType(MemberType.ADMIN);
+
+            em.persist(member2);
+
             em.flush();
             em.clear();
 
-//            String query = "SELECT CASE WHEN m.age <= 10 THEN '학생요금' " +
-//                           "            WHEN m.age >= 60 THEN '경로요금' " +
-//                           "            ELSE '일반요금' " +
-//                           "            END" +
-//                           " FROM Member m";
-//            String query = "SELECT COALESCE(m.username, '이름 없는 회원') FROM Member m";
-            String query = "SELECT NULLIF(m.username, '관리자') FROM Member m";
+//            String query = "SELECT FUNCTION('GROUP_CONCAT', m.username) FROM Member m";
+            String query = "SELECT GROUP_CONCAT(m.username) FROM Member m";
             List<String> result = em.createQuery(query, String.class).getResultList();
             for (String s : result) {
                 System.out.println(s);
