a258ef5c5961f25465b5d19c9ac6ecd976751922
kk920106 <kk920106@naver.com>
2024-05-11 13:32:14 +0900

    [견고한 결제 시스템 구축] 결제 복구 서비스 (feat: Bulk Head, Parallel Processing)

--- yeojeongmin/safe-payment/payment-service/src/main/kotlin/com/example/paymentservice/payment/adapter/out/persistent/PaymentPersistentAdapter.kt
+++ yeojeongmin/safe-payment/payment-service/src/main/kotlin/com/example/paymentservice/payment/adapter/out/persistent/PaymentPersistentAdapter.kt
@@ -4,11 +4,10 @@ import com.example.paymentservice.common.PersistentAdapter
 import com.example.paymentservice.payment.adapter.out.persistent.repository.PaymentRepository
 import com.example.paymentservice.payment.adapter.out.persistent.repository.PaymentStatusUpdateRepository
 import com.example.paymentservice.payment.adapter.out.persistent.repository.PaymentValidationRepository
-import com.example.paymentservice.payment.application.port.out.PaymentStatusUpdateCommand
-import com.example.paymentservice.payment.application.port.out.PaymentStatusUpdatePort
-import com.example.paymentservice.payment.application.port.out.PaymentValidationPort
-import com.example.paymentservice.payment.application.port.out.SavePaymentPort
+import com.example.paymentservice.payment.application.port.out.*
 import com.example.paymentservice.payment.domain.PaymentEvent
+import com.example.paymentservice.payment.domain.PendingPaymentEvent
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @PersistentAdapter
@@ -16,8 +15,9 @@ class PaymentPersistentAdapter(
     private val paymentRepository: PaymentRepository,
     private val paymentStatusUpdateRepository: PaymentStatusUpdateRepository,
     private val paymentValidationRepository: PaymentValidationRepository,
-//) : SavePaymentPort {
-) : SavePaymentPort, PaymentStatusUpdatePort, PaymentValidationPort {
+//) : SavePaymentPort, PaymentStatusUpdatePort, PaymentValidationPort {
+) : SavePaymentPort, PaymentStatusUpdatePort, PaymentValidationPort, LoadPendingPaymentPort {
+
     override fun save(paymentEvent: PaymentEvent): Mono<Void> {
         return paymentRepository.save(paymentEvent)
     }
@@ -26,11 +26,15 @@ class PaymentPersistentAdapter(
         return paymentStatusUpdateRepository.updatePaymentStatusToExecuting(orderId, paymentKey)
     }
 
+    override fun isValid(orderId: String, amount: Long): Mono<Boolean> {
+        return paymentValidationRepository.isValid(orderId, amount)
+    }
+
     override fun updatePaymentStatus(command: PaymentStatusUpdateCommand): Mono<Boolean> {
         return paymentStatusUpdateRepository.updatePaymentStatus(command)
     }
 
-    override fun isValid(orderId: String, amount: Long): Mono<Boolean> {
-        return paymentValidationRepository.isValid(orderId, amount)
+    override fun getPendingPayments(): Flux<PendingPaymentEvent> {
+        return paymentRepository.getPendingPayments()
     }
 }
--- yeojeongmin/safe-payment/payment-service/src/main/kotlin/com/example/paymentservice/payment/application/port/in/PaymentConfirmCommand.kt
+++ yeojeongmin/safe-payment/payment-service/src/main/kotlin/com/example/paymentservice/payment/application/port/in/PaymentConfirmCommand.kt
@@ -1,7 +1,7 @@
 package com.example.paymentservice.payment.application.port.`in`
 
-data class PaymentConfirmCommand (
-  val paymentKey: String,
-  val orderId: String,
-  val amount: Long
+data class PaymentConfirmCommand(
+    val paymentKey: String,
+    val orderId: String,
+    val amount: Long
 )
