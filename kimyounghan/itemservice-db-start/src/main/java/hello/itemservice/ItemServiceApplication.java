3a97d4ffee74093d646fa2e227befeab87b64833
kk920106 <kk920106@naver.com>
2024-09-07 19:07:40 +0900

    [스프링 DB 2편 - 데이터 접근 활용 기술] JdbcTemplate 적용3 - 구성과 실행

--- kimyounghan/itemservice-db-start/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ kimyounghan/itemservice-db-start/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
-
-@Import(MemoryConfig.class)
+@Import(JdbcTemplateV1Config.class)
+//@Import(MemoryConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 	public static void main(String[] args) {
