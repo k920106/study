66955cad7b6d90796cae3cac4c909b0cc623a038
kk920106 <kk920106@naver.com>
2024-09-14 12:00:57 +0900

    [스프링 DB 2편 - 데이터 접근 활용 기술] 스프링 데이터 JPA 적용2

--- kimyounghan/itemservice-db-start/src/main/java/hello/itemservice/service/ItemService.java
+++ kimyounghan/itemservice-db-start/src/main/java/hello/itemservice/service/ItemService.java
@@ -8,12 +8,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ItemService {
-
     Item save(Item item);
-
     void update(Long itemId, ItemUpdateDto updateParam);
-
     Optional<Item> findById(Long id);
-
     List<Item> findItems(ItemSearchCond itemSearch);
 }
