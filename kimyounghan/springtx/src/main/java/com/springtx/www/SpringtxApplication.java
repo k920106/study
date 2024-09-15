52f61b1656010da6f29b92eba0aef628aa3097cd
kk920106 <kk920106@naver.com>
2024-09-15 09:36:16 +0900

    [스프링 DB 2편 - 데이터 접근 활용 기술] 프로젝트 생성

---
+++ kimyounghan/springtx/src/main/java/com/springtx/www/SpringtxApplication.java
@@ -0,0 +1,11 @@
+package com.springtx.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtxApplication.class, args);
+    }
+}
