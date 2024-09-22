3b6edaf81e50f258674477e0b03a813ec84d333b
kk920106 <kk920106@naver.com>
2024-09-22 17:56:16 +0900

    [스프링 핵심 원리 - 고급편] 로그 추적기 V1 - 프로토타입 개발

---
+++ kimyounghan/advanced/src/test/java/com/advanced/www/app/trace/hellotrace/HelloTraceV1Test.java
@@ -0,0 +1,20 @@
+package com.advanced.www.app.trace.hellotrace;
+
+import com.advanced.www.app.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+class HelloTraceV1Test {
+    @Test
+    void begin_end() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.end(status);
+    }
+
+    @Test
+    void begin_exception() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.exception(status, new IllegalStateException());
+    }
+}
