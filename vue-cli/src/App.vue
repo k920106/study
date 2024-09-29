d8529acb20e287d1a857d36ce984a793e45d9ebb
kk920106 <kk920106@naver.com>
2024-09-29 13:44:43 +0900

    싱글 파일 컴포넌트에서 props 속성 사용하는 방법

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <app-header></app-header>
+  <!-- <app-header></app-header> -->
+  <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+  <app-header v-bind:propsdata="str"></app-header>
 </template>
 
 <script>
@@ -8,7 +10,8 @@ import AppHeader from './components/AppHeader.vue'
 export default {
   data: function() {
     return {
-      str: 'hi'
+      // str: 'hi'
+      str: 'Header'
     }
   },
   components: {
