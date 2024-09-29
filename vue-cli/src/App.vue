cba85f5dedfabd0971e66a2245cb0ffd6be15c88
kk920106 <kk920106@naver.com>
2024-09-29 13:34:26 +0900

    싱글 파일 컴포넌트 체계에서 컴포넌트 등록하기

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,13 +1,18 @@
 <template>
-  <div>{{ str }}</div>
+  <app-header></app-header>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue'
+
 export default {
   data: function() {
     return {
       str: 'hi'
     }
+  },
+  components: {
+    'app-header': AppHeader
   }
 }
 </script>
