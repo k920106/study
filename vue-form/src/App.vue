f98b8bcde15ffa7ee89c6e4bef2a833e4809ae98
kk920106 <kk920106@naver.com>
2024-09-29 15:20:18 +0900

    프로젝트 생성 및 마크업 작업

---
+++ vue-form/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <form action="">
+    <div>
+      <label for="username">id: </label>
+      <input type="text" id="username">
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input type="text" id="password">
+    </div>
+    <button>login</button>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
