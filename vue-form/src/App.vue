c3916e94d00a000ece27d9b12caad361231f82d2
kk920106 <kk920106@naver.com>
2024-09-29 15:35:22 +0900

    v-model 속성과 submit 이벤트 처리

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,20 +1,33 @@
 <template>
-  <form action="">
+  <!-- <form action=""> -->
+  <form v-on:submit="submitForm">
     <div>
       <label for="username">id: </label>
-      <input type="text" id="username">
+      <input type="text" id="username" v-model="username">
     </div>
     <div>
       <label for="password">pw: </label>
-      <input type="text" id="password">
+      <input type="text" id="password" v-model="password">
     </div>
-    <button>login</button>
+    <!-- <button>login</button> -->
+    <button type="submit">login</button>
   </form>
 </template>
 
 <script>
 export default {
-
+  data: function() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitForm: function(event) {
+      event.preventDefault();
+      console.log(this.username, this.password);
+    }
+  }
 }
 </script>
 
