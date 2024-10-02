d33acd67b3175711adbf9a130fc032680bcd2ec8
kk920106 <kk920106@naver.com>
2024-10-03 06:51:14 +0900

    [스프링 핵심 원리 - 고급편] 로그 추적기 V1 - 적용

---
+++ kimyounghan/advanced/src/main/java/com/advanced/www/app/v1/OrderServiceV1.java
@@ -0,0 +1,26 @@
+package com.advanced.www.app.v1;
+
+import com.advanced.www.app.trace.TraceStatus;
+import com.advanced.www.app.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV1 {
+    private final OrderRepositoryV1 orderRepository;
+    private final HelloTraceV1 trace;
+
+    public void orderItem(String itemId) {
+        // orderRepository.save(itemId);
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderService.orderItem()");
+            orderRepository.save(itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
