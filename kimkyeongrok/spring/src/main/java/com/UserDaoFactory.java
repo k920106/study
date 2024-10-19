a2ff714228223fe67d54b0c480c485a5848ed7ac
kk920106 <kk920106@naver.com>
2024-10-19 17:06:19 +0900

    [Spring framework 개발 환경 구축하기와 Dao 리팩토링하기] spring 적용하기 (1)

--- kimkyeongrok/spring/src/main/java/com/UserDaoFactory.java
+++ kimkyeongrok/spring/src/main/java/com/UserDaoFactory.java
@@ -1,12 +1,18 @@
 package com;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UserDaoFactory {
-    public UserDao getLocalUserDao() {
+    @Bean
+    public UserDao localUserDao() {
         ConnectionMaker connectionMaker = new LocalConnectionMaker();
         return new UserDao(connectionMaker);
     }
 
-    public UserDao getPrdUserDao() {
+    @Bean
+    public UserDao prdUserDao() {
         ConnectionMaker connectionMaker = new PrdConnectionMaker();
         return new UserDao(connectionMaker);
     }
--- kimkyeongrok/spring/src/main/java/com/UserDaoTest.java
+++ kimkyeongrok/spring/src/main/java/com/UserDaoTest.java
@@ -1,24 +1,28 @@
 package com;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws SQLException {
-        UserDaoFactory userDaoFactory = new UserDaoFactory();
-        UserDao dao = userDaoFactory.getLocalUserDao();
+        ApplicationContext context;
+        context = new AnnotationConfigApplicationContext(UserDaoFactory.class);
+        UserDao dao = context.getBean("localUserDao", UserDao.class);
 
         String environment = "prd";
         if (environment.equals("prd")) {
-            dao = userDaoFactory.getPrdUserDao();
+            dao = context.getBean("prdUserDao", UserDao.class);
         }
 
         User user = new User();
-        user.setId("12345678");
+        user.setId("1");
         user.setPassword("kms1234");
         user.setName("kms");
         dao.add(user);
 
-        User selectedUser = dao.get("12345678");
+        User selectedUser = dao.get("1");
         System.out.println("id: " + selectedUser.getId());
         System.out.println("password: " + selectedUser.getPassword());
         System.out.println("name: " + selectedUser.getName());
