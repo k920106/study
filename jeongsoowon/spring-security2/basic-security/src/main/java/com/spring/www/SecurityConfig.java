25d1310483f72f565a50cab524d6676c89ce560a
kk920106 <kk920106@naver.com>
2024-10-20 15:46:03 +0900

    [스프링 시큐리티] 5) Logout 처리, LogoutFilter

--- jeongsoowon/spring-security2/basic-security/src/main/java/com/spring/www/SecurityConfig.java
+++ jeongsoowon/spring-security2/basic-security/src/main/java/com/spring/www/SecurityConfig.java
@@ -5,13 +5,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Configuration
@@ -20,26 +20,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated();
-        http.formLogin()
-            .loginPage("/loginPage")
-            .defaultSuccessUrl("/")
-            .failureUrl("/login")
-            .usernameParameter("userId")
-            .passwordParameter("passwd")
-            .loginProcessingUrl("/login_proc")
-            .successHandler(new AuthenticationSuccessHandler() {
+        http.formLogin();
+        http.logout()
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/login")
+            .addLogoutHandler(new LogoutHandler() {
                 @Override
-                public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
-                    System.out.println("authentication : " + authentication.getName());
-                    httpServletResponse.sendRedirect("/");
-                }})
-            .failureHandler(new AuthenticationFailureHandler() {
+                public void logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) {
+                    HttpSession session = httpServletRequest.getSession();
+                    session.invalidate();
+                }
+            })
+            .logoutSuccessHandler(new LogoutSuccessHandler() {
                 @Override
-                public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
-                    System.out.println("exception : " + e.getMessage());
+                public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
                     httpServletResponse.sendRedirect("/login");
-                }})
-            .permitAll()
-        ;
+                }
+            })
+            .deleteCookies("remember-me");
     }
 }
