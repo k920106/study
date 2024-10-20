d08a5a26bc17eb098262cb7250223fdac52c7a87
kk920106 <kk920106@naver.com>
2024-10-20 16:11:58 +0900

    [스프링 시큐리티] 9) 동시 세션 제어, 세션 고정 보호, 세션 정책

--- jeongsoowon/spring-security2/basic-security/src/main/java/com/spring/www/SecurityConfig.java
+++ jeongsoowon/spring-security2/basic-security/src/main/java/com/spring/www/SecurityConfig.java
@@ -1,23 +1,20 @@
 package com.spring.www;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private UserDetailsService userDetailsService;
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated();
         http.formLogin();
-        http.rememberMe()
-            .userDetailsService(userDetailsService);
+        http.sessionManagement()
+            .sessionFixation()
+            .changeSessionId()
+        ;
     }
 }
