4123c0952e494b75ad6802187659d357e4f6d10f
kk920106 <kk920106@naver.com>
2024-10-26 08:07:37 +0900

    [스프링 시큐리티] 인텔리제이 오류 해결

---
+++ jeongsoowon/spring-security2/basic-security/src/main/java/com/spring/www/BasicSecurityApplication.java
@@ -0,0 +1,11 @@
+package com.spring.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BasicSecurityApplication.class, args);
+    }
+}
