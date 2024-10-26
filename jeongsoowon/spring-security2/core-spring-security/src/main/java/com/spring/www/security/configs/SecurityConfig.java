10827954a8dc5bcd89cb09d3048b16a6d2306b09
kk920106 <kk920106@naver.com>
2024-10-26 12:59:20 +0900

    [스프링 시큐리티] 5) DB 연동 인증 처리(2) : CustomAuthenticationProvider

--- jeongsoowon/spring-security2/core-spring-security/src/main/java/com/spring/www/security/configs/SecurityConfig.java
+++ jeongsoowon/spring-security2/core-spring-security/src/main/java/com/spring/www/security/configs/SecurityConfig.java
@@ -1,35 +1,36 @@
 package com.spring.www.security.configs;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.spring.www.security.provider.CustomAuthenticationProvider;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private UserDetailsService userDetailsService;
+    //@Autowired private UserDetailsService userDetailsService;
 
     //@Override
     //protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    //    String password = passwordEncoder().encode("1111");
-    //    auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
-    //    auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER");
-    //    auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
+    //    auth.userDetailsService(userDetailsService);
     //}
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        return new CustomAuthenticationProvider();
     }
 
     @Bean
