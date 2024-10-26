0c5dd31e67a86819b0fe38737862868d1c973d21
kk920106 <kk920106@naver.com>
2024-10-26 19:49:52 +0900

    [스프링 시큐리티] 4) 인증 핸들러 - AjaxAuthenticationSuccessHandler, AjaxAuthenticationFailureHandler

--- jeongsoowon/spring-security2/core-spring-security/src/main/java/com/spring/www/security/configs/AjaxSecurityConfig.java
+++ jeongsoowon/spring-security2/core-spring-security/src/main/java/com/spring/www/security/configs/AjaxSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.spring.www.security.configs;
 
 import com.spring.www.security.filter.AjaxLoginProcessingFilter;
+import com.spring.www.security.handler.AjaxAuthenticationFailureHandler;
+import com.spring.www.security.handler.AjaxAuthenticationSuccessHandler;
 import com.spring.www.security.provider.AjaxAuthenticationProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,11 +11,23 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @Order(0)
 public class AjaxSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public AuthenticationSuccessHandler ajaxAuthenticationSuccessHandler() {
+        return new AjaxAuthenticationSuccessHandler();
+    }
+
+    @Bean
+    public AuthenticationFailureHandler ajaxAuthenticationFailureHandler() {
+        return new AjaxAuthenticationFailureHandler();
+    }
+
     @Bean
     public AuthenticationProvider ajaxAuthenticationProvider() {
         return new AjaxAuthenticationProvider();
@@ -23,6 +37,8 @@ public class AjaxSecurityConfig extends WebSecurityConfigurerAdapter {
     public AjaxLoginProcessingFilter ajaxLoginProcessingFilter() throws Exception {
         AjaxLoginProcessingFilter ajaxLoginProcessingFilter = new AjaxLoginProcessingFilter();
         ajaxLoginProcessingFilter.setAuthenticationManager(authenticationManagerBean());
+        ajaxLoginProcessingFilter.setAuthenticationSuccessHandler(ajaxAuthenticationSuccessHandler());
+        ajaxLoginProcessingFilter.setAuthenticationFailureHandler(ajaxAuthenticationFailureHandler());
         return ajaxLoginProcessingFilter;
     }
 
