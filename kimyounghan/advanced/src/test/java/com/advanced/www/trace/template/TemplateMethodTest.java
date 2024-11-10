09d33929a588a43fb21dede6aab575622ec9ea8b
kk920106 <kk920106@naver.com>
2024-11-10 21:35:30 +0900

    [스프링 핵심 원리 - 고급편] 템플릿 메서드 패턴 - 예제2

--- kimyounghan/advanced/src/test/java/com/advanced/www/trace/template/TemplateMethodTest.java
+++ kimyounghan/advanced/src/test/java/com/advanced/www/trace/template/TemplateMethodTest.java
@@ -1,5 +1,8 @@
 package com.advanced.www.trace.template;
 
+import com.advanced.www.trace.template.code.AbstractTemplate;
+import com.advanced.www.trace.template.code.SubClassLogic1;
+import com.advanced.www.trace.template.code.SubClassLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -13,9 +16,9 @@ public class TemplateMethodTest {
 
     private void logic1() {
         long startTime = System.currentTimeMillis();
-        //비즈니스 로직 실행
+        // 비즈니스 로직 실행
         log.info("비즈니스 로직1 실행");
-        //비즈니스 로직 종료
+        // 비즈니스 로직 종료
         long endTime = System.currentTimeMillis();
         long resultTime = endTime - startTime;
         log.info("resultTime={}", resultTime);
@@ -29,5 +32,17 @@ public class TemplateMethodTest {
         long endTime = System.currentTimeMillis();
         long resultTime = endTime - startTime;
         log.info("resultTime={}", resultTime);
+    }
+
+    /**
+     * 템플릿 메서드 패턴 적용
+     */
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
     }
 }
