b8f8e65cd7ca5da3aec6b3c15463203efbde7a0f
kk920106 <kk920106@naver.com>
2024-11-11 08:01:45 +0900

    [스프링 핵심 원리 - 고급편] 템플릿 메서드 패턴 - 적용1

---
+++ kimyounghan/advanced/src/main/java/com/advanced/www/trace/template/AbstractTemplate.java
@@ -0,0 +1,30 @@
+package com.advanced.www.trace.template;
+
+import com.advanced.www.trace.TraceStatus;
+import com.advanced.www.trace.logtrace.LogTrace;
+
+public abstract class AbstractTemplate<T> {
+    private final LogTrace trace;
+
+    public AbstractTemplate(LogTrace trace) {
+        this.trace = trace;
+    }
+
+    public T execute(String message) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin(message);
+
+            T result = call(); // 로직 호출
+
+            trace.end(status);
+
+            return result;
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+    protected abstract T call();
+}
