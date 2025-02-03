f46b1c9b834567bf5bdc4364b90eb50e755dd470
kk920106 <kk920106@naver.com>
2025-02-04 00:00:35 +0900

    한 입 크기로 잘라먹는 Next.js(v15)

--- leejeonghwan/next/section02/src/pages/index.tsx
+++ leejeonghwan/next/section02/src/pages/index.tsx
@@ -1,3 +1,7 @@
+//import "./index.css";
+import style from "./index.module.css"
+
 export default function Home() {
-  return <h1>인덱스</h1>;
+  //return <h1 style={{color: "red"}}>인덱스</h1>;
+  return <h1 className={style.h1}>인덱스</h1>;
 }
