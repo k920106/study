cb8b19857bd69d3e2a783661ca28925b60bdd175
kk920106 <kk920106@naver.com>
2025-02-09 20:15:58 +0900

    한 입 크기로 잘라먹는 Next.js(v15)

--- leejeonghwan/next/section02/src/pages/index.tsx
+++ leejeonghwan/next/section02/src/pages/index.tsx
@@ -1,11 +1,28 @@
 import style from "./index.module.css"
-import {ReactNode} from "react";
+import {ReactNode, useEffect} from "react";
 import SearchableLayout from "@/components/searchable-layout";
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import {InferGetServerSidePropsType} from "next";
 
-export default function Home() {
-	// return <h1 className={style.h1}>인덱스</h1>;
+export const getServerSideProps = () => {
+	// 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
+	const data = "hello";
+
+	return {
+		props: {
+			data,
+		},
+	};
+};
+
+export default function Home({data}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+	console.log(data)
+
+	useEffect(() => {
+		console.log(window.location)
+	}, []);
+
 	return (
 			<div className={style.container}>
 				<section>
