e4bce0ec015e2a7fb8ee3a3383854a278af61581
kk920106 <kk920106@naver.com>
2025-03-02 21:07:54 +0900

    한 입 크기로 잘라먹는 Next.js(v15)

--- leejeonghwan/next/section03/src/app/(with-searchbar)/search/page.tsx
+++ leejeonghwan/next/section03/src/app/(with-searchbar)/search/page.tsx
@@ -1,18 +1,18 @@
-import ClientComponent from "@/components/client-component";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
-export default async function Page({
-		searchParams,
+export default function Page({
+  searchParams,
 }: {
-	searchParams: Promise<{q: string}>
+  searchParams: Promise<{
+    q?: string;
+  }>;
 }) {
-	const { q } = await searchParams;
-
-	return (
-			<div>
-				Search 페이지 : {q}
-				<ClientComponent>
-					<></>
-				</ClientComponent>
-			</div>
-	)
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
