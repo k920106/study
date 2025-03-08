ba1c4aca62495b78e5218f758ce438066ba35ae2
kk920106 <kk920106@naver.com>
2025-03-08 12:30:15 +0900

    한 입 크기로 잘라먹는 Next.js(v15)

--- leejeonghwan/next/section06/src/app/(with-searchbar)/page.tsx
+++ leejeonghwan/next/section06/src/app/(with-searchbar)/page.tsx
@@ -1,16 +1,12 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
 import {BookData} from "@/types";
-
-// export const dynamic = "auto";
-// export const dynamic = "force-dynamic";
-// 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
-// 1. auto: 기본 값, 아무것도 강제하지 않음
-// 2. force-dynamic : 페이지를 강제로 Dynamic 페이지로 설정
-// 3. force-static : 페이지를 강제로 Static 페이지로 설정
-// 4. error : 페이지를 강제로 Static 페이지 설정 (설정하면 안되는 이유 -> 빌드 오류)
+import {delay} from "@/util/delay";
+import {Suspense} from "react";
 
 async function AllBooks() {
+  await delay(1500);
+
   const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
       { cache: "force-cache" }
@@ -31,6 +27,8 @@ async function AllBooks() {
 }
 
 async function RecoBooks() {
+  await delay(1000);
+
   const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
       { next: { revalidate: 3 } }
@@ -50,16 +48,22 @@ async function RecoBooks() {
   )
 }
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <RecoBooks />
+        <Suspense fallback={<div>추천하는 도서 Loading...</div>}>
+          <RecoBooks />
+        </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <AllBooks />
+        <Suspense fallback={<div>등록된 도서 Loading...</div>}>
+          <AllBooks />
+        </Suspense>
       </section>
     </div>
   );
--- leejeonghwan/next/section06/src/app/(with-searchbar)/search/page.tsx
+++ leejeonghwan/next/section06/src/app/(with-searchbar)/search/page.tsx
@@ -1,14 +1,9 @@
 import BookItem from "@/components/book-item";
 import {BookData} from "@/types";
 import {delay} from "@/util/delay";
+import {Suspense} from "react";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{q: string}>
-}) {
-  const { q } = await searchParams;
-
+async function SearchResult({ q }: { q: string }) {
   await delay(1500);
   const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
@@ -21,10 +16,24 @@ export default async function Page({
   const books: BookData[] = await response.json();
 
   return (
-    <div>
-      {books.map((book) => (
-        <BookItem key={book.id} {...book} />
-      ))}
-    </div>
+      <div>
+        {books.map((book) => (
+            <BookItem key={book.id} {...book} />
+        ))}
+      </div>
   );
+}
+
+export default function Page({
+  searchParams,
+}: {
+  searchParams: {q: string}
+}) {
+  const { q } = searchParams;
+
+  return (
+      <Suspense key={q} fallback={<div>Loading...</div>}>
+        <SearchResult q={q} />
+      </Suspense>
+  )
 }
