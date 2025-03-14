1df22809ef8c3994d90f91cf88af3f0e15257298
kk920106 <kk920106@naver.com>
2025-03-14 23:51:55 +0900

    한 입 크기로 잘라먹는 Next.js(v15)

--- leejeonghwan/next/section07/src/actions/create-review.action.ts
+++ leejeonghwan/next/section07/src/actions/create-review.action.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { revalidatePath } from "next/cache";
+import { revalidateTag } from "next/cache";
 
 export async function createReviewAction(formData: FormData) {
 	const bookId = formData.get("bookId")?.toString();
@@ -19,8 +19,22 @@ export async function createReviewAction(formData: FormData) {
 					body: JSON.stringify({bookId, content, author}),
 				}
 		);
-		console.log(response.statusText)
-		revalidatePath(`/book/${bookId}`);
+		console.log(response.status);
+
+		// 1. 특정 주소의 해당하는 페이지만 재검증
+		// revalidatePath(`/book/${bookId}`);
+
+		// 2. 특정 경로의 모든 동적 페이지를 재검증
+		// revalidatePath("/book/[id]", "page");
+
+		// 3. 특정 레이아웃을 갖는 모든 페이지 재검증
+		// revalidatePath("/(with-searchbar)", "layout");
+
+		// 4. 모든 데이터 재검증
+		// revalidatePath("/", "layout");
+
+		// 5. 태그 기준, 데이터 캐시 재검증
+		revalidateTag(`review-${bookId}`);
 	} catch (err) {
 		console.error(err);
 		return;
