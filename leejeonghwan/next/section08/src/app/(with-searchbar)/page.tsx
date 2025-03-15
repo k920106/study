e6f9689aeb76178ce2e8a6d737354a4f55f31514
kk920106 <kk920106@naver.com>
2025-03-15 22:58:52 +0900

    한 입 크기로 잘라먹는 Next.js(v15)

--- leejeonghwan/next/section08/src/app/(with-searchbar)/page.tsx
+++ leejeonghwan/next/section08/src/app/(with-searchbar)/page.tsx
@@ -4,68 +4,79 @@ import {BookData} from "@/types";
 import {delay} from "@/util/delay";
 import {Suspense} from "react";
 import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
+import {Metadata} from "next";
 
 async function AllBooks() {
-  await delay(1000);
+	await delay(1000);
 
-  const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
-      { cache: "force-cache" }
-  );
-  if (!response.ok) {
-    return <div>오류가 발생했습니다...</div>;
-  }
+	const response = await fetch(
+			`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+			{cache: "force-cache"}
+	);
+	if (!response.ok) {
+		return <div>오류가 발생했습니다...</div>;
+	}
 
-  const allBooks: BookData[] = await response.json();
+	const allBooks: BookData[] = await response.json();
 
-  return (
-      <div>
-        {allBooks.map((book) => (
-            <BookItem key={book.id} {...book} />
-        ))}
-      </div>
-  )
+	return (
+			<div>
+				{allBooks.map((book) => (
+						<BookItem key={book.id} {...book} />
+				))}
+			</div>
+	)
 }
 
 async function RecoBooks() {
-  await delay(1000);
+	await delay(1000);
 
-  const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
-      // { next: { revalidate: 3 } }
-  );
-  if (!response.ok) {
-    return <div>오류가 발생했습니다...</div>;
-  }
+	const response = await fetch(
+			`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+			// { next: { revalidate: 3 } }
+	);
+	if (!response.ok) {
+		return <div>오류가 발생했습니다...</div>;
+	}
 
-  const recoBooks: BookData[] = await response.json();
+	const recoBooks: BookData[] = await response.json();
 
-  return (
-      <div>
-        {recoBooks.map((book) => (
-            <BookItem key={book.id} {...book} />
-        ))}
-      </div>
-  )
+	return (
+			<div>
+				{recoBooks.map((book) => (
+						<BookItem key={book.id} {...book} />
+				))}
+			</div>
+	)
 }
 
 export const dynamic = "force-dynamic";
 
+export const metadata: Metadata = {
+	title: "한입 북스",
+	description: "한입 북스에 등록된 도서를 만나보세요",
+	openGraph: {
+		title: "한입 북스",
+		description: "한입 북스에 등록된 도서를 만나보세요",
+		images: ['/thumbnail.png'],
+	}
+}
+
 export default async function Home() {
-  return (
-    <div className={style.container}>
-      <section>
-        <h3>지금 추천하는 도서</h3>
-        <Suspense fallback={<BookListSkeleton count={3} />}>
-        <RecoBooks />
-        </Suspense>
-      </section>
-      <section>
-        <h3>등록된 모든 도서</h3>
-        <Suspense fallback={<BookListSkeleton count={10} />}>
-          <AllBooks />
-        </Suspense>
-      </section>
-    </div>
-  );
+	return (
+			<div className={style.container}>
+				<section>
+					<h3>지금 추천하는 도서</h3>
+					<Suspense fallback={<BookListSkeleton count={3}/>}>
+						<RecoBooks/>
+					</Suspense>
+				</section>
+				<section>
+					<h3>등록된 모든 도서</h3>
+					<Suspense fallback={<BookListSkeleton count={10}/>}>
+						<AllBooks/>
+					</Suspense>
+				</section>
+			</div>
+	);
 }
--- leejeonghwan/next/section08/src/app/(with-searchbar)/search/page.tsx
+++ leejeonghwan/next/section08/src/app/(with-searchbar)/search/page.tsx
@@ -2,6 +2,7 @@ import BookItem from "@/components/book-item";
 import {BookData} from "@/types";
 import {delay} from "@/util/delay";
 import {Suspense} from "react";
+import {Metadata} from "next";
 
 async function SearchResult({q}: { q: string }) {
 	await delay(1500);
@@ -22,6 +23,24 @@ async function SearchResult({q}: { q: string }) {
 				))}
 			</div>
 	);
+}
+
+export async function generateMetadata({
+	searchParams
+}: {
+	searchParams: Promise<{ q: string }>
+// }) {
+}): Promise<Metadata> {
+	const {q} = await searchParams;
+	return {
+		title: `${q}: 한입북스 검색`,
+		description: `${q}의 검색 결과입니다`,
+		openGraph: {
+			title: `${q}: 한입북스 검색`,
+			description: `${q}의 검색 결과입니다`,
+			images: ['/thumbnail.png'],
+		}
+	}
 }
 
 export default async function Page({
