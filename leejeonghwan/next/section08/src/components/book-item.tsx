097ab4f97f16d856bb299ad1ca43d4463e82a55b
kk920106 <kk920106@naver.com>
2025-03-15 13:21:04 +0900

    한 입 크기로 잘라먹는 Next.js(v15)

---
+++ leejeonghwan/next/section08/src/components/book-item.tsx
@@ -0,0 +1,27 @@
+import type { BookData } from "@/types";
+import Link from "next/link";
+import style from "./book-item.module.css";
+import Image from "next/image";
+
+export default function BookItem({
+  id,
+  title,
+  subTitle,
+  description,
+  author,
+  publisher,
+  coverImgUrl,
+}: BookData) {
+  return (
+    <Link href={`/book/${id}`} className={style.container}>
+      <Image src={coverImgUrl} alt={""} width={80} height={100} />
+      <div>
+        <div className={style.title}>{title}</div>
+        <div className={style.subTitle}>{subTitle}</div>
+        <div className={style.author}>
+          {author} | {publisher}
+        </div>
+      </div>
+    </Link>
+  );
+}
---
+++ leejeonghwan/next/section08/src/components/review-editor.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import style from "./review-editor.module.css";
+import {createReviewAction} from "@/actions/create-review.action";
+import {useActionState, useEffect} from "react";
+
+export default function ReviewEditor({bookId}: { bookId: string }) {
+
+	const [state, formAction, isPending] = useActionState(
+			createReviewAction,
+			null
+	);
+
+	useEffect(() => {
+		if (state && !state.status) {
+			alert(state.error)
+		}
+	}, [state]);
+
+	return (
+			<section>
+				<form
+						className={style.form_container}
+						action={formAction}
+				>
+					<input name="bookId" value={bookId} hidden readOnly/>
+					<textarea
+							name="content"
+							placeholder="리뷰 내용"
+							required
+							disabled={isPending}
+					/>
+					<div className={style.submit_container}>
+						<input
+								name="author"
+								placeholder="작성자"
+								required
+								disabled={isPending}
+						/>
+						<button
+								type="submit"
+								disabled={isPending}
+						>
+							{isPending ? "..." : "작성하기"}
+						</button>
+					</div>
+				</form>
+			</section>
+	);
+}
