0b3e661eb6af3f4a2048f1c6ea77ddb262158396
kk920106 <kk920106@naver.com>
2025-04-03 23:56:24 +0900

    7개 프로젝트로 완벽 대비하는 Next.js 실무 (ft. 성능 개선)

--- fastcampus/fastcampus-nextbnb/components/Map/index.tsx
+++ fastcampus/fastcampus-nextbnb/components/Map/index.tsx
@@ -43,11 +43,21 @@ export default function Map() {
 				// 마커가 표시될 위치입니다
 				const markerPosition = new window.kakao.maps.LatLng(room.lat, room.lng)
 				// 마커를 생성합니다
-				const marker = new window.kakao.maps.Marker({
-					position: markerPosition,
-				})
+				// const marker = new window.kakao.maps.Marker({
+				// 	position: markerPosition,
+				// })
 				// 마커가 지도 위에 표시되도록 설정합니다
-				marker.setMap(map)
+				// marker.setMap(map)
+				// custom overlay를 설정해줍니다
+				const content = `<div class="custom_overlay">${room.price?.toLocaleString()}원</div>`
+
+				// custom overlay를 생성합니다
+				const customOverlay = new window.kakao.maps.CustomOverlay({
+					position: markerPosition,
+					content: content,
+				})
+				// 커스텀 오버레이가 지도 위에 표시되도록 설정합니다
+				customOverlay.setMap(map)
 			})
 		})
 	}
@@ -76,4 +86,3 @@ export function MapButton({ onClick }: { onClick: () => void }) {
 			</button>
 	)
 }
-
