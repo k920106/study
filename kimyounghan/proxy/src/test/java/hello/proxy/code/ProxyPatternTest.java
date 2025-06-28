0e6133c3ba8ad83288a378909e042748c38ed7de
kk920106 <kk920106@naver.com>
2025-06-28 15:02:12 +0900

    스프링 핵심 원리 - 고급편

---
+++ kimyounghan/proxy/src/test/java/hello/proxy/code/ProxyPatternTest.java
@@ -0,0 +1,14 @@
+package hello.proxy.code;
+
+import org.junit.jupiter.api.Test;
+
+public class ProxyPatternTest {
+    @Test
+    void noProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+}
