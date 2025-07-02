42837b9266c7c2ef07edf09d0e1f90455354fc73
kk920106 <kk920106@naver.com>
2025-07-02 21:45:05 +0900

    스프링 핵심 원리 - 고급편

--- kimyounghan/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ kimyounghan/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,6 +1,6 @@
 package hello.proxy;
 
-import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -8,10 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import(InterfaceProxyConfig.class)
-//@Import({AppV1Config.class, AppV2Config.class})
+@Import(ConcreteProxyConfig.class)
+//@Import(InterfaceProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(ProxyApplication.class, args);
     }
