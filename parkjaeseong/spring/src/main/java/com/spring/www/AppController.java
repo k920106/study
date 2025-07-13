91c57dbf863c8b48c015a74fbaf99608fd117921
kk920106 <kk920106@naver.com>
2025-07-13 23:50:48 +0900

    비전공자도 이해할 수 있는 Docker 입문/실전

---
+++ parkjaeseong/spring/src/main/java/com/spring/www/AppController.java
@@ -0,0 +1,12 @@
+package com.spring.www;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello Spring World";
+    }
+}
