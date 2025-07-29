8759d93415a3ff979b3100cfb3e9db86dc6e8e3b
kk920106 <kk920106@naver.com>
2025-07-29 23:35:53 +0900

    7개 프로젝트로 완벽 대비하는 Next.js 실무 (ft. 성능 개선)

--- kimhayeon/nextbnb/next.config.js
+++ kimhayeon/nextbnb/next.config.js
@@ -17,4 +17,9 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+// module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig)
